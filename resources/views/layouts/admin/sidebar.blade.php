facf4d925e234ba7a089e1740925841ff01e7013
samir <samir.parikh@drcsystems.com>
2025-03-18 17:26:46 +0530

    Role & Permission Menu Created

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -53,7 +53,7 @@
                 <a class="nav-link" data-bs-toggle="collapse" href="#rolesPermission" role="button" aria-expanded="false"
                     aria-controls="rolesPermission">
                     <i class="link-icon" data-feather="settings"></i>
-                    <span class="link-title">Administration</span>
+                    <span class="link-title">Role & Permission</span>
                     <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
                 <div class="collapse" id="rolesPermission">
